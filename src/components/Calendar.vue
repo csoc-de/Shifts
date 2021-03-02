67c842d94202598d2271d83ffb700b68798bfd1b
fabian.kirchesch <fabian.kirchesch@csoc.de>
2021-03-02 10:54:35 +0100

    --changing mutiple Date Selects, not done yet

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -10,8 +10,31 @@ import { Plugin as TimeLinePointer } from 'gantt-schedule-timeline-calendar/dist
 import { Plugin as Selection } from 'gantt-schedule-timeline-calendar/dist/plugins/selection.esm.min.js'
 import { Plugin as ItemMovement } from 'gantt-schedule-timeline-calendar/dist/plugins/item-movement.esm.min.js'
 import 'gantt-schedule-timeline-calendar/dist/style.css'
+import { getYYYYMMDDFromDate } from '../utils/date'
 
 let gstc, state
+
+function getBackground(shiftType) {
+	const hash = hashCode(shiftType)
+
+	const c = (hash & 0x00FFFFFF)
+		.toString(16)
+		.toUpperCase()
+
+	return '#' + '00000'.substring(0, 6 - c.length) + c
+}
+function hashCode(string) {
+	let hash = 0
+	let i
+	let chr
+	if (string.length === 0) return hash
+	for (i = 0; i < string.length; i++) {
+		chr = string.charCodeAt(i)
+		hash = ((hash << 5) - hash) + chr
+		hash |= 0
+	}
+	return hash
+}
 
 export default {
 	name: 'Calendar',
@@ -33,7 +56,6 @@ export default {
 	watch: {
 		shifts: {
 			handler(newVal) {
-				console.log('handler', state)
 				const difference = newVal
 					.filter(x => !this.currentShifts.includes(x))
 					.concat(this.currentShifts.filter(x => !newVal.includes(x)))
@@ -49,6 +71,9 @@ export default {
 							start: start.valueOf(),
 							end: start.endOf('day').valueOf(),
 						},
+						style: {
+							background: getBackground(shift.shiftsType.name),
+						},
 					}
 					state.update(`config.chart.items.${id}`, newItem)
 				})
@@ -59,6 +84,7 @@ export default {
 	},
 	mounted() {
 		this.currentShifts = this.shifts.slice()
+		const today = GSTC.api.date(getYYYYMMDDFromDate(new Date()))
 		const config = {
 			licenseKey: '====BEGIN LICENSE KEY====\\navmUbYOBJJQTbPqKzglx+pD9L4wzHfJIE6LW4LXOOVsKzjPTHZejg6f47UyJrTjDvVpk1EqhdhR3xSzHmnI9MlYyg+xmqh7XmPv+evL7Xs5KnQmO7tma3GpHP9+IKMyPcaKb56GM+Y4DHwCn+AWcT/5KIZ3st8AyH/bvarWsyZnKyKbDR/D/byxLVV2inuLi55CNPwqBbmaQwTwix3DLdpRY9LrTuN2GxRwBdj02I78wQEyw8D8uJjSIAsJ7/9pZpQaMKmCVYdaVd/tMYm+LDBCxLPYIgQ0+f0QXbWLhwIxLjZT7T+hgxij/W3bARpfhpND+UlntBXyF02qLBU8ETw==||U2FsdGVkX1+gMjgB+GK2W/+cQ/JFsPpcewUdI8eIqA+qbYM+N8XWWFjxfjvmQr4g8qggXZV0l3ArT8A3l6Ib6tZBAC4u+T38wpSpeTW581E=\\nsQXfjVJtIl9hd9HAXYawCwLOye+yJWgxkB0sMGcGFpuaG1Gsp73bCMRqY4sEBiJOblLBb07k4xg+ZfgLHAui/DjI3fezDxDlWOinfOZawQcrydGn+WOvknd9FMotwxI5JsISa6Y/p2lpOuAF2nyrQRgUde04TXPKomf7+uuP/314pidXoVE9x0OY1Aysd2S69JGPly8OSEewvAz9v6cP2ET4MGIFsxyyhpu0CnyEnNlgeQLpYctRAoVQ5fPi3mD8yXqbNR6NuPWtO9qrw2VgYgf0+X6tupt9ElHvErsJ2Ww0fpfdgRAgaG2AfCg1/MGpYqDi6/IH3xfjLltm6QPqKw==\\n====END LICENSE KEY====',
 			plugins: [TimeLinePointer(), Selection(), ItemMovement()],
@@ -67,15 +93,7 @@ export default {
 					data: {
 						[GSTC.api.GSTCID('id')]: {
 							id: GSTC.api.GSTCID('id'),
-							width: 200,
-							data: ({ row }) => GSTC.api.sourceID(row.id),
-							header: {
-								content: 'ID',
-							},
-						},
-						[GSTC.api.GSTCID('id')]: {
-							id: GSTC.api.GSTCID('id'),
-							width: 200,
+							width: 100,
 							data: 'label',
 							header: {
 								content: 'Analyst',
@@ -87,6 +105,11 @@ export default {
 			},
 			chart: {
 				items: GSTC.api.fromArray(this.generateItems()),
+				time: {
+					from: today.startOf('month').valueOf(),
+					to: today.endOf('month').valueOf(),
+					calculatedZoomMode: true,
+				},
 			},
 		}
 		state = GSTC.api.stateFromConfig(config)
@@ -94,7 +117,6 @@ export default {
 			element: this.$refs.gstc,
 			state,
 		})
-		console.log(state)
 	},
 
 	beforeUnmount() {
@@ -130,6 +152,9 @@ export default {
 						end: start.endOf('day').valueOf(),
 					},
 					minWidth: 200,
+					style: {
+						background: getBackground(shiftsType.name),
+					},
 				})
 			})
 			return items
--- src/views/NewShift.vue
+++ src/views/NewShift.vue
@@ -49,7 +49,13 @@
 			@change="updateShiftType" />
 		<!-- eslint-disable -->
 		<vc-date-picker
-			v-model="newShiftInstance.date"
+			ref="datePicker"
+			:value="today"
+			:attributes="attributes"
+			:popover-keepVisibleOnInput="true"
+			:popover-visibility="visibility"
+			mode="date"
+			is-inline
 			@dayclick="onDayClick">
 			<template v-slot="{ inputValue, togglePopover}">
 				<div class="divOpenDatepicker">
@@ -60,9 +66,6 @@
 							<path fill="currentColor" d="M9,10V12H7V10H9M13,10V12H11V10H13M17,10V12H15V10H17M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5A2,2 0 0,1 5,3H6V1H8V3H16V1H18V3H19M19,19V8H5V19H19M9,14V16H7V14H9M13,14V16H11V14H13M17,14V16H15V14H17Z" />
 						</svg>
 					</button>
-					<input
-						:value="inputValue"
-						class="inputOpenDatepicker bg-white text-gray-700 w-full py-1 px-2 appearance-none border rounded-r focus:outline-none focus:border-blue-500"/>
 				</div>
 			</template>
 		</vc-date-picker>
@@ -108,10 +111,15 @@ export default {
 				id: -1,
 				name: 'Select ShiftType',
 			},
+			visibility: 'visible',
 			newShiftInstance: {
 				analysts: [],
 				shiftsType: '',
-				date: new Date(),
+				dates: [
+					{
+						date: new Date(),
+					},
+				],
 			},
 		}
 	},
@@ -124,13 +132,42 @@ export default {
 				this.newShiftInstance.date = newValue
 			},
 		},
+		dates() {
+			return this.newShiftInstance.dates.map(day => day.date)
+		},
+		attributes() {
+			return this.dates.map(date => ({
+				highlight: true,
+				dates: date,
+			}))
+		},
+		today() {
+			return {
+				highlight: false,
+				dates: new Date(),
+			}
+		},
 	},
 	mounted() {
 		this.isLoading = false
+		this.$nextTick(() => {
+			console.log(this.$refs.datePicker)
+		})
 	},
 	methods: {
 		onDayClick(date) {
-			this.newShiftInstance.date = date
+			console.log(date)
+			console.log(this.newShiftInstance.dates)
+			const idx = this.newShiftInstance.dates.findIndex((d) => d.id === date.id)
+			if (idx >= 0) {
+				this.newShiftInstance.dates.splice(idx, 1)
+			} else {
+				this.newShiftInstance.dates.push({
+					id: date.id,
+					date: date.date,
+				})
+			}
+			this.visibility = 'visible'
 		},
 		closeEditor() {
 			this.$emit('cancel')
@@ -157,7 +194,7 @@ export default {
 			this.newShiftInstance = {
 				analysts: [],
 				shiftsType: '',
-				date: new Date(),
+				dates: [],
 			}
 			this.value1 = {
 				id: -1,
