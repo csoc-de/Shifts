37266cbcb32ce370d0a76d22e1a9c2ba11e5e464
Kevin Küchler <kevin.kuechler@csoc.de>
2023-07-31 11:10:27 +0200

    Bumped version

--- src/components/Navigation/AppNavigation.vue
+++ src/components/Navigation/AppNavigation.vue
@@ -35,7 +35,7 @@
 				to="/archive" />
 		</template>
 		<template #footer>
-			<span style="margin-left: 10px">Version: 1.9.0-dev</span>
+			<span style="margin-left: 10px">Version: 1.9.1</span>
 		</template>
 	</AppNavigationVue>
 </template>
