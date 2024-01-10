bc20a6487059e31d8cec4e1c3437995a0865aa2d
Kevin Küchler <kevin.kuechler@csoc.de>
2024-01-10 07:22:45 +0100

    Updated version after bugfix

--- src/components/Navigation/AppNavigation.vue
+++ src/components/Navigation/AppNavigation.vue
@@ -35,7 +35,7 @@
 				to="/archive" />
 		</template>
 		<template #footer>
-			<span style="margin-left: 10px">Version: 1.9.5</span>
+			<span style="margin-left: 10px">Version: 1.9.6</span>
 		</template>
 	</AppNavigationVue>
 </template>
